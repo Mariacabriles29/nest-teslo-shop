da2c1383abe38bfb54c05ea0f81b1a533e6222f2
mariacabriles29 <mecp2992@gmail.com>
2023-12-12 14:50:29 -0400

    feat: create columns of the entities

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -5,9 +5,40 @@ export class Product {
   //se declaran las variables con las propiedades de typeorm
   @PrimaryGeneratedColumn('uuid')
   id: string;
-
+  //se usa el deoorador column para definir las columnas
   @Column('text', {
     unique: true,
   })
   title: string;
+
+  @Column('numeric', {
+    default: 0,
+  })
+  price: number;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  slug: string;
+
+  @Column('int', {
+    default: 0,
+  })
+  stock: number;
+  @Column('text', {
+    array: true,
+  })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
+
+  //tags
+  //images
 }
