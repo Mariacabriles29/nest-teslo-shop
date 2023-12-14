a66789d7ad43c98525e4496e811e0233ae9f8c8c
mariacabriles29 <mecp2992@gmail.com>
2023-12-14 15:32:06 -0400

    feat: creation of the ProductImage entity

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
+import { Product, ProductImage } from './entities';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   //para poder usar la entidad de los products debo importarla aqui
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
 })
 export class ProductsModule {}
