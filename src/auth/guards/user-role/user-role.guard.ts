5ef312cdaf4e3f2bf3d4f179c32e1601a89ade1f
mariacabriles29 <mecp2992@gmail.com>
2023-12-19 20:57:12 -0400

    feat: checking user roles with a nest metadata decorator

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -2,10 +2,13 @@ import {
   BadRequestException,
   CanActivate,
   ExecutionContext,
+  ForbiddenException,
   Injectable,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { fork } from 'child_process';
 import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
 //este guard se va encargar de ver el usuario y revisar si tiene sus roles
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -21,8 +24,20 @@ export class UserRoleGuard implements CanActivate {
       context.getHandler(),
     );
 
-    console.log({ validRoles });
+    if (!validRoles) return true;
+    if (validRoles.length === 0) return true;
 
-    return true;
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+    if (!user) throw new BadRequestException('User not found');
+    console.log({ userRoles: user.roles });
+    //realizo un forof para verificar los roles
+    for (const role of user.roles) {
+      if (validRoles.includes(role)) return true;
+    }
+
+    throw new ForbiddenException(
+      `User ${user.fullName} need a valid role: [${validRoles}]`,
+    );
   }
 }
