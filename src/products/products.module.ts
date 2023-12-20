9fa01d5269b3f7dff0830734adcf170b596af1a8
mariacabriles29 <mecp2992@gmail.com>
2023-12-20 09:43:27 -0400

    feat: The Auth decorator was added for authentication and authorization in some modules

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,12 +3,13 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product, ProductImage } from './entities';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   //para poder usar la entidad de los products debo importarla aqui
-  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage]), AuthModule],
   exports: [ProductsService],
 })
 export class ProductsModule {}
