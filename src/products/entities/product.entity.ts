cba1cc73139e6c7b234e4e97dc4d50348e092152
mariacabriles29 <mecp2992@gmail.com>
2023-12-21 12:03:04 -0400

    feat: The api response and api property were added to the products

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -9,48 +9,54 @@ import {
 } from 'typeorm';
 import { ProductImage } from './product-image.entity';
 import { User } from '../../auth/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'products' })
 export class Product {
+  @ApiProperty()
   //se declaran las variables con las propiedades de typeorm
   @PrimaryGeneratedColumn('uuid')
   id: string;
   //se usa column para definir las columnas
+  @ApiProperty()
   @Column('text', {
     unique: true,
   })
   title: string;
-
+  @ApiProperty()
   @Column('float', {
     default: 0,
   })
   price: number;
-
+  @ApiProperty()
   @Column({
     type: 'text',
     nullable: true,
   })
   description: string;
-
+  @ApiProperty()
   @Column('text', {
     unique: true,
   })
   slug: string;
-
+  @ApiProperty()
   @Column('int', {
     default: 0,
   })
   stock: number;
+
+  @ApiProperty()
   @Column('text', {
     array: true,
   })
   sizes: string[];
 
+  @ApiProperty()
   @Column('text')
   gender: string;
 
   //tags
-
+  @ApiProperty()
   @Column('text', {
     array: true,
     default: [],
@@ -58,6 +64,7 @@ export class Product {
   tags: string[];
 
   //relacion uno a mucho un producto puede tener muchas images
+  @ApiProperty()
   @OneToMany(() => ProductImage, (productImage) => productImage.product, {
     cascade: true,
     eager: true,
