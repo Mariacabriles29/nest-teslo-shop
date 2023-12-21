e64b8350e6b939a40fd212f3d9430929b099a035
mariacabriles29 <mecp2992@gmail.com>
2023-12-21 11:36:04 -0400

    feat: swagger configuration to start endpoint documentation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
 import { Logger, ValidationPipe } from '@nestjs/common';
-import express from 'express';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +17,13 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Teslo RESTFul API')
+    .setDescription('The shop endpoints')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT);
   logger.log(`App running on port ${process.env.PORT}`);
